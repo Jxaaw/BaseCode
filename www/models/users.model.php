e9ba09b0ca1eb006f7d9b5ea973ae478e9eb20b5
Jxaaw <valentinw21@gmail.com>
2020-04-27 11:38:01 +0200

    Exo 1

--- www/models/users.model.php
+++ www/models/users.model.php
@@ -9,6 +9,16 @@ class users extends DB
     protected $pwd;
     protected $status;
 
+    public function hydrate(array $donnees){
+        foreach($donnees as $key =>$value)
+        {
+            $method='set'.ucfirst($key);
+            if(method_exists($this,$method))
+            {
+                return $this->$method($value);
+            }
+        }
+    }
 
     public function __construct()
     {
